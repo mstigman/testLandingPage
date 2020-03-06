4611e0a5ce2875a5ebe794059b24980d5b63f427
mstigman <mstigman@gmail.com>
2020-03-06 12:12:39 -0500

    created header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import React, { Component } from 'react';
 import Header from "./components/header";
 import Main from "./components/main";
 import Footer from "./components/footer";
+import Nav from "./components/nav";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-       
-        <Header />
-        <Main />
-        <Footer />
+          <Nav />
       </div>
     );
   }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import logo from "../images/KayNLogo.svg";
 
 class Nav extends Component {
   render() {
     return (
-      <nav>
-          <ul>
-              <li className="logo">Design<span>Labs</span></li>
+      <nav style={{backgroundColor: '#83e5e8', height: 60, flexDirection: 'row'}}>
+          <ul style={{height: 70, width: 70, paddingLeft: 15, backgroundColor: '#aaa0'}}>
+            <img src={logo}/>
           </ul>
-          <ul>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Mission</a></li>
-              <li><a href="#">Contact</a></li>
+          <ul style={{fontSize: 50}}>
+            <li className="logo">KayN</li>
+          </ul>
+          <ul/>
+          <ul style={{}}>
+              <li style={{fontSize: 30}}><a href="PrivacyPolicy">Home</a></li>
+              <li style={{fontSize: 30}}><a href="#">About</a></li>
+              <li style={{fontSize: 30}}><a href="#">Contact</a></li>
           </ul>
       </nav>
     );
